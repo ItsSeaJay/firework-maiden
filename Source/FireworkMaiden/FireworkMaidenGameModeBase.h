a74f0744acf941ba1e435fc730700fcf39595380
Callum <callum@ischus.co.uk>
2020-01-28 17:50:33 +0000

    Add project files

---
+++ Source/FireworkMaiden/FireworkMaidenGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FireworkMaidenGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIREWORKMAIDEN_API AFireworkMaidenGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
